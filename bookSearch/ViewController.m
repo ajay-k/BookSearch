2005a4fcaf07a883a580e1591788936870405c10
Your Name <ajay_k@sbcglobal.net>
2018-12-16 18:07:21 -0800

    Pulled book information including book name, author, description successfully

--- bookSearch/ViewController.m
+++ bookSearch/ViewController.m
@@ -24,25 +24,70 @@
 - (void) fetchBooksUsingJSON {
     NSLog(@"Fetching Books");
     
-    NSString *urlString = @"https://itunes.apple.com/lookup?id=909253";
+    NSString *urlString = @"https://itunes.apple.com/search?term=fitzgerald&entity=ebook&limit=1";
+    //NSString *urlString = @"https://api.letsbuildthatapp.com/jsondecodable/courses";
+
     
     NSURL *url = [NSURL URLWithString:urlString];
     
     [[NSURLSession.sharedSession dataTaskWithURL:(url) completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         
-        NSLog(@"Finished fetching books");
+       NSLog(@"Finished fetching books");
         
-        NSString *dummyString = [[NSString alloc] initWithData:(data) encoding:(NSUTF8StringEncoding)];
+       //NSString *dummyString = [[NSString alloc] initWithData:(data) encoding:(NSUTF8StringEncoding)];
         
-        NSLog(@"Dummy string: %@", dummyString);
+       //NSLog(@"Dummy string: %@", dummyString);
         
         NSError *err;
         
-        NSArray *booksJSON = [NSJSONSerialization JSONObjectWithData:(data) options:NSJSONReadingAllowFragments error:(&err)];
+        //NSArray *booksJSON = [NSJSONSerialization JSONObjectWithData:(data) options:NSJSONReadingAllowFragments error:(&err)];
+        
+        NSDictionary *booksJSON = [NSJSONSerialization JSONObjectWithData:(data) options:NSJSONReadingAllowFragments error:(&err)];
+        
         
         if (err) {
             NSLog(@"Failed to serialzie into JSON: %@", err);
             return;
+        }
+        
+        
+       // NSArray *arrayOfJSON = [allDataDictionary objectForKey:@"trackName"];
+
+        //create dict and save data and display it on console
+        
+        for (NSString *tempObject in booksJSON ) {
+           // NSLog(@"Object: %@", [booksJSON objectForKey:tempObject]);
+            //NSString *trackName = booksJSON[@"trackName"];
+            //NSLog(@"%@",[booksJSON objectForKey:@"result"]);
+            //NSLog(trackName);
+            
+            //NSLog(@"Object: %@, Key: %@", [booksJSON valueForKey:tempObject], tempObject);
+        }
+        
+        //NSLog(@"DEC");
+
+        for (NSDictionary *bookDict in booksJSON[@"results"]) {
+            //Book detail should at least the display the title, author, summary and an image.
+            
+            //NSString *trackName = [bookDict objectForKey:@"resultCount"];
+            //NSString *trackName = bookDict[@"trackName"];
+            //NSLog(trackName);
+            
+//                NSString *firstName = bookDict[@"trackName"];
+//                NSString *lastName = bookDict[@"artistName"];
+            
+            
+            id trackName = bookDict[@"trackName"];
+            id artistName = bookDict[@"artistName"];
+            id description = bookDict[@"description"];
+
+
+            NSLog(trackName);
+            NSLog(artistName);
+            NSLog(description);
+
+
+            //NSLog(@"TrackName: %@", bookDict[@"trackName"]);
         }
     }] resume];
 }
